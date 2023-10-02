e4c29d40071a60000ed385faf36c9db40d7f55e6
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-10-02 19:35:54 -0300

    feat: asset handling

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as facebook } from './facebook.png'
+export { default as instagram } from './instagram.png'
+export { default as tiktok } from './tik-tok.png'
+export { default as github } from './github.png'
+export { default as linkedin } from './linkedin.png'
+export { default as twitter } from './twitter.png'
+export { default as youtube } from './youtube.png'
+export { default as userAvatar } from './user_avatar.png'
