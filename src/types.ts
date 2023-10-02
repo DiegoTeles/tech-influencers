f3608c82a8a79ac72d28342b94666f14efa44b81
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-10-02 19:36:07 -0300

    feat: type files

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface SocialProps {
+  link: string
+  path: string
+  username: string
+}
+
+export interface PersonProps {
+  name: string
+  username: string
+  photo: string
+  role: string
+}
+
+export interface SocialNetworksProps {
+  facebook: string
+  instagram: string
+  linkedin: string
+  tiktok: string
+  github: string
+  twitter: string
+  youtube: string
+}
